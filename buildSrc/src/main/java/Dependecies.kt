39ac12d141154c756f734a45243b06da04dd62ee
Kamil Gurgul <kamgurgul@gmail.com>
2018-02-20 10:10:59 +0100

    Change versioning and add kotlin deps

---
+++ buildSrc/src/main/java/Dependecies.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017 KG Soft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+object Versions {
+    // Build sdk
+    val minSdk = 14
+    val targetSdk = 27
+    val compiledSdk = 27
+
+    // Kotlin
+    val kotlin = "1.2.21"
+    val anko = "0.10.1"
+
+    // Libs
+    val androidSupport = "27.0.2"
+    val androidDatabinding = "2.3.0"
+    val firebase = "11.8.0"
+    val archLifecycle = "1.1.0"
+    val dagger = "2.13"
+    val glide = "4.3.1"
+    val espresso = "3.0.1"
+}
