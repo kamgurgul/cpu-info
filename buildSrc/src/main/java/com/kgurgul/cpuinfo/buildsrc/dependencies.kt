4da83f08285884aff49933fc97e837eab6cbf5ca
Kamil Gurgul <kamgurgul@gmail.com>
2021-11-02 10:29:00 +0100

    Add compose

--- buildSrc/src/main/java/com/kgurgul/cpuinfo/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/kgurgul/cpuinfo/buildsrc/dependencies.kt
@@ -57,6 +57,17 @@ object Libs {
             const val safeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:$version"
         }
 
+        object Compose {
+            const val version = "1.0.4"
+            const val activity = "androidx.activity:activity-compose:1.4.0"
+            const val material = "androidx.compose.material:material:$version"
+            const val animations = "androidx.compose.animation:animation:$version"
+            const val uiTooling = "androidx.compose.ui:ui-tooling:$version"
+            const val themeAdapter = "com.google.android.material:compose-theme-adapter:$version"
+            const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0"
+            const val uiTests = "androidx.compose.ui:ui-test-junit4:$version"
+        }
+
         object Test {
             private const val version = "1.4.0"
             const val core = "androidx.test:core:$version"
