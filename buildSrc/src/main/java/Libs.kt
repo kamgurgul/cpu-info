1e341cfa9ffa162b16c0e16ed8e7cd0faa79960b
Kamil Gurgul <kamgurgul@gmail.com>
2023-05-24 12:27:23 +0200

    Coil upgrade

--- buildSrc/src/main/java/Libs.kt
+++ buildSrc/src/main/java/Libs.kt
@@ -12,7 +12,7 @@ object Libs {
 
     const val junit = "junit:junit:4.13.2"
 
-    const val coil = "io.coil-kt:coil-compose:2.3.0"
+    const val coil = "io.coil-kt:coil-compose:2.4.0"
 
     object Kotlin {
         private const val version = "1.8.21"
