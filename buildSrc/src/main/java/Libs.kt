d6a980b8164d16850b53b3eb119bd7827861d927
Kamil Gurgul <kamgurgul@gmail.com>
2023-07-28 10:16:09 +0200

    Update dependencies

--- buildSrc/src/main/java/Libs.kt
+++ buildSrc/src/main/java/Libs.kt
@@ -21,7 +21,7 @@ object Libs {
     }
 
     object Coroutines {
-        private const val version = "1.7.2"
+        private const val version = "1.7.3"
         const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
         const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
         const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
@@ -30,9 +30,9 @@ object Libs {
     object AndroidX {
         const val coreKtx = "androidx.core:core-ktx:1.9.0"
         const val activityKtx = "androidx.activity:activity-compose:1.7.2"
-        const val fragmentKtx = "androidx.fragment:fragment-ktx:1.6.0"
+        const val fragmentKtx = "androidx.fragment:fragment-ktx:1.6.1"
         const val appCompat = "androidx.appcompat:appcompat:1.6.1"
-        const val preference = "androidx.preference:preference:1.2.0"
+        const val preference = "androidx.preference:preference:1.2.1"
         const val swipeRefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
         const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.4"
         const val multiDex = "androidx.multidex:multidex:2.0.1"
@@ -55,7 +55,7 @@ object Libs {
         }
 
         object Compose {
-            const val compilerVersion = "1.5.0"
+            const val compilerVersion = "1.5.1"
             const val bom = "androidx.compose:compose-bom:2023.06.01"
             const val material = "androidx.compose.material:material"
             const val material3 = "androidx.compose.material3:material3"
