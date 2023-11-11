31977549f2025dc56c298c76491c3b03d1dc38f8
Kamil Gurgul <kamgurgul@gmail.com>
2023-11-11 11:03:28 +0100

    Settings VM implementation

--- app/src/main/java/com/kgurgul/cpuinfo/features/settings/SettingsViewModel.kt
+++ app/src/main/java/com/kgurgul/cpuinfo/features/settings/SettingsViewModel.kt
@@ -1,11 +1,40 @@
 package com.kgurgul.cpuinfo.features.settings
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kgurgul.cpuinfo.data.local.UserPreferencesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
-
+    private val userPreferencesRepository: UserPreferencesRepository,
 ) : ViewModel() {
+
+    val uiStateFlow = userPreferencesRepository.userPreferencesFlow
+        .mapLatest { userPreferences ->
+            UiState(
+                temperatureUnit = userPreferences.temperatureUnit,
+                theme = userPreferences.theme,
+            )
+        }
+
+    fun setTemperatureUnit(temperatureUnit: Int) {
+        viewModelScope.launch {
+            userPreferencesRepository.setTemperatureUnit(temperatureUnit)
+        }
+    }
+
+    fun setTheme(theme: String) {
+        viewModelScope.launch {
+            userPreferencesRepository.setTheme(theme)
+        }
+    }
+
+    data class UiState(
+        val temperatureUnit: Int,
+        val theme: String,
+    )
 }
