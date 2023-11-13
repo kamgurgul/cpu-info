b51b6ce6abc67ade20386c84eba9f551dc0d1f75
Kamil Gurgul <kamgurgul@gmail.com>
2023-11-13 08:13:18 +0100

    Add temperature dialog to settings

--- app/src/main/java/com/kgurgul/cpuinfo/features/settings/SettingsScreen.kt
+++ app/src/main/java/com/kgurgul/cpuinfo/features/settings/SettingsScreen.kt
@@ -1,19 +1,34 @@
 package com.kgurgul.cpuinfo.features.settings
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kgurgul.cpuinfo.R
+import com.kgurgul.cpuinfo.features.temperature.TemperatureFormatter
 import com.kgurgul.cpuinfo.ui.components.PrimaryTopAppBar
 import com.kgurgul.cpuinfo.ui.theme.CpuInfoTheme
+import com.kgurgul.cpuinfo.ui.theme.spacingMedium
+import kotlinx.collections.immutable.ImmutableList
 
 @Composable
 fun SettingsScreen(
@@ -22,12 +37,16 @@ fun SettingsScreen(
     val uiState by viewModel.uiStateFlow.collectAsStateWithLifecycle()
     SettingsScreen(
         uiState = uiState,
+        onTemperatureDialogDismissRequest = viewModel::onTemperatureDialogDismissed,
+        onTemperatureOptionClicked = viewModel::setTemperatureUnit,
     )
 }
 
 @Composable
 fun SettingsScreen(
     uiState: SettingsViewModel.UiState,
+    onTemperatureDialogDismissRequest: () -> Unit,
+    onTemperatureOptionClicked: (Int) -> Unit,
 ) {
     Scaffold(
         topBar = {
@@ -42,6 +61,12 @@ fun SettingsScreen(
         SettingsList(
             uiState = uiState,
             modifier = paddingModifier,
+        )
+        TemperatureUnitDialog(
+            onDismissRequest = onTemperatureDialogDismissRequest,
+            currentSelection = uiState.temperatureUnit,
+            options = uiState.temperatureDialogOptions,
+            onOptionClicked = onTemperatureOptionClicked,
         )
     }
 }
@@ -58,12 +83,75 @@ private fun SettingsList(
     }
 }
 
+@Composable
+private fun TemperatureUnitDialog(
+    onDismissRequest: () -> Unit,
+    currentSelection: Int,
+    options: ImmutableList<Int>?,
+    onOptionClicked: (Int) -> Unit,
+) {
+    if (options != null) {
+        AlertDialog(
+            onDismissRequest = onDismissRequest,
+            tonalElevation = 0.dp,
+            title = {
+                Text(text = stringResource(id = R.string.temperature_unit))
+            },
+            text = {
+                val scrollState = rememberScrollState()
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(spacingMedium),
+                    modifier = Modifier.verticalScroll(scrollState),
+                ) {
+                    for (option in options) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    onOptionClicked(option)
+                                    onDismissRequest()
+                                }
+                        ) {
+                            RadioButton(
+                                selected = option == currentSelection,
+                                onClick = null,
+                            )
+                            val text = when (option) {
+                                TemperatureFormatter.CELSIUS ->
+                                    stringResource(id = R.string.celsius)
+
+                                TemperatureFormatter.FAHRENHEIT ->
+                                    stringResource(id = R.string.fahrenheit)
+
+                                TemperatureFormatter.KELVIN ->
+                                    stringResource(id = R.string.kelvin)
+
+                                else -> throw IllegalArgumentException("Unknown temperature unit")
+                            }
+                            Text(text = text)
+                        }
+                    }
+                }
+            },
+            confirmButton = {
+                Button(
+                    onClick = onDismissRequest
+                ) {
+                    Text(text = stringResource(id = R.string.cancel))
+                }
+            }
+        )
+    }
+}
+
 @Preview
 @Composable
 fun SettingsScreenPreview() {
     CpuInfoTheme {
         SettingsScreen(
-            uiState = SettingsViewModel.UiState()
+            uiState = SettingsViewModel.UiState(),
+            onTemperatureDialogDismissRequest = {},
+            onTemperatureOptionClicked = {},
         )
     }
 }
