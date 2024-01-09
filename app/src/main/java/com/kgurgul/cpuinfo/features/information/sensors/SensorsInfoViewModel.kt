7f36a6d5326191e757e2dc414adda0c9273f9245
Kamil Gurgul <kamgurgul@gmail.com>
2024-01-09 07:09:48 +0100

    Handle different type of sensors

--- app/src/main/java/com/kgurgul/cpuinfo/features/information/sensors/SensorsInfoViewModel.kt
+++ app/src/main/java/com/kgurgul/cpuinfo/features/information/sensors/SensorsInfoViewModel.kt
@@ -20,6 +20,8 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.hardware.TriggerEvent
+import android.hardware.TriggerEventListener
 import androidx.lifecycle.ViewModel
 import com.kgurgul.cpuinfo.domain.model.SensorData
 import com.kgurgul.cpuinfo.utils.round1
@@ -55,11 +57,22 @@ class SensorsInfoViewModel @Inject constructor(
         }
         Thread {
             for (sensor in sensorList) {
-                sensorManager.registerListener(
-                    this,
-                    sensor,
-                    SensorManager.SENSOR_DELAY_NORMAL
-                )
+                if (sensor.reportingMode == Sensor.REPORTING_MODE_ONE_SHOT) {
+                    sensorManager.requestTriggerSensor(
+                        object : TriggerEventListener() {
+                            override fun onTrigger(event: TriggerEvent) {
+                                updateSensorInfo(event.sensor, event.values)
+                            }
+                        },
+                        sensor
+                    )
+                } else {
+                    sensorManager.registerListener(
+                        this,
+                        sensor,
+                        SensorManager.SENSOR_DELAY_NORMAL
+                    )
+                }
             }
         }.start()
     }
@@ -75,24 +88,20 @@ class SensorsInfoViewModel @Inject constructor(
     }
 
     override fun onSensorChanged(event: SensorEvent) {
-        updateSensorInfo(event)
+        updateSensorInfo(event.sensor, event.values)
     }
 
-    /**
-     * Replace sensor value with the new one
-     */
-    @Synchronized
-    private fun updateSensorInfo(event: SensorEvent) {
-        val updatedRowId = sensorList.indexOf(event.sensor)
+    private fun updateSensorInfo(sensor: Sensor, values: FloatArray) {
+        val updatedRowId = sensorList.indexOf(sensor)
         _uiStateFlow.update { uiState ->
             uiState.copy(
                 sensors = uiState.sensors.toMutableList().apply {
                     set(
                         updatedRowId,
                         SensorData(
-                            id = event.sensor.getUniqueId(),
-                            name = event.sensor.name,
-                            value = getSensorData(event),
+                            id = sensor.getUniqueId(),
+                            name = sensor.name,
+                            value = getSensorData(sensor.type, values),
                         )
                     )
                 }
@@ -104,90 +113,90 @@ class SensorsInfoViewModel @Inject constructor(
      * Detect sensor type for passed [SensorEvent] and format it to the correct unit
      */
     @Suppress("DEPRECATION")
-    private fun getSensorData(event: SensorEvent): String {
+    private fun getSensorData(sensorType: Int, values: FloatArray): String {
         var data = " "
 
-        when (event.sensor.type) {
+        when (sensorType) {
             Sensor.TYPE_ACCELEROMETER, Sensor.TYPE_GRAVITY, Sensor.TYPE_LINEAR_ACCELERATION ->
-                data = "X=${event.values[0].round1()}m/s²  Y=${
-                    event.values[1].round1()
-                }m/s²  Z=${event.values[2].round1()}m/s²"
+                data = "X=${values[0].round1()}m/s²  Y=${
+                    values[1].round1()
+                }m/s²  Z=${values[2].round1()}m/s²"
 
             Sensor.TYPE_GYROSCOPE ->
-                data = "X=${event.values[0].round1()}rad/s  Y=${
-                    event.values[1].round1()
-                }rad/s  Z=${event.values[2].round1()}rad/s"
+                data = "X=${values[0].round1()}rad/s  Y=${
+                    values[1].round1()
+                }rad/s  Z=${values[2].round1()}rad/s"
 
             Sensor.TYPE_ROTATION_VECTOR ->
-                data = "X=${event.values[0].round1()}  Y=${
-                    event.values[1].round1()
-                }  Z=${event.values[2].round1()}"
+                data = "X=${values[0].round1()}  Y=${
+                    values[1].round1()
+                }  Z=${values[2].round1()}"
 
             Sensor.TYPE_MAGNETIC_FIELD ->
-                data = "X=${event.values[0].round1()}μT  Y=${
-                    event.values[1].round1()
-                }μT  Z=${event.values[2].round1()}μT"
+                data = "X=${values[0].round1()}μT  Y=${
+                    values[1].round1()
+                }μT  Z=${values[2].round1()}μT"
 
             Sensor.TYPE_ORIENTATION ->
-                data = "Azimuth=${event.values[0].round1()}°  Pitch=${
-                    event.values[1].round1()
-                }°  Roll=${event.values[2].round1()}°"
+                data = "Azimuth=${values[0].round1()}°  Pitch=${
+                    values[1].round1()
+                }°  Roll=${values[2].round1()}°"
 
             Sensor.TYPE_PROXIMITY ->
-                data = "Distance=${event.values[0].round1()}cm"
+                data = "Distance=${values[0].round1()}cm"
 
             Sensor.TYPE_AMBIENT_TEMPERATURE,
             GOOGLE_GYRO_TEMPERATURE_SENSOR_TYPE,
             GOOGLE_PRESSURE_TEMPERATURE_SENSOR_TYPE ->
-                data = "Temperature=${event.values[0].round1()}°C"
+                data = "Temperature=${values[0].round1()}°C"
 
             Sensor.TYPE_LIGHT ->
-                data = "Illuminance=${event.values[0].round1()}lx"
+                data = "Illuminance=${values[0].round1()}lx"
 
             Sensor.TYPE_PRESSURE ->
-                data = "Air pressure=${event.values[0].round1()}hPa"
+                data = "Air pressure=${values[0].round1()}hPa"
 
             Sensor.TYPE_RELATIVE_HUMIDITY ->
-                data = "Relative humidity=${event.values[0].round1()}%"
+                data = "Relative humidity=${values[0].round1()}%"
 
             Sensor.TYPE_TEMPERATURE ->
-                data = "Device temperature=${event.values[0].round1()}°C"
+                data = "Device temperature=${values[0].round1()}°C"
 
             Sensor.TYPE_GYROSCOPE_UNCALIBRATED ->
-                data = "X=${event.values[0].round1()}rad/s  Y=${
-                    event.values[1].round1()
+                data = "X=${values[0].round1()}rad/s  Y=${
+                    values[1].round1()
                 }rad/s  Z=${
-                    event.values[2].round1()
+                    values[2].round1()
                 }rad/s\nEstimated drift: X=${
-                    event.values[3].round1()
+                    values[3].round1()
                 }rad/s  Y=${
-                    event.values[4].round1()
+                    values[4].round1()
                 }rad/s  Z=${
-                    event.values[5].round1()
+                    values[5].round1()
                 }rad/s"
 
             Sensor.TYPE_GAME_ROTATION_VECTOR ->
-                data = "X=${event.values[0].round1()}  Y=${
-                    event.values[1].round1()
-                }  Z=${event.values[2].round1()}"
+                data = "X=${values[0].round1()}  Y=${
+                    values[1].round1()
+                }  Z=${values[2].round1()}"
 
             Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED ->
-                data = "X=${event.values[0].round1()}μT  Y=${
-                    event.values[1].round1()
+                data = "X=${values[0].round1()}μT  Y=${
+                    values[1].round1()
                 }μT  Z=${
-                    event.values[2].round1()
+                    values[2].round1()
                 }μT\nIron bias: X=${
-                    event.values[3].round1()
+                    values[3].round1()
                 }μT  Y=${
-                    event.values[4].round1()
+                    values[4].round1()
                 }μT  Z=${
-                    event.values[5].round1()
+                    values[5].round1()
                 }μT"
 
             Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR ->
-                data = "X=${event.values[0].round1()}  Y=${
-                    event.values[1].round1()
-                }  Z=${event.values[2].round1()}"
+                data = "X=${values[0].round1()}  Y=${
+                    values[1].round1()
+                }  Z=${values[2].round1()}"
         }
 
         return data
