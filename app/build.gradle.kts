45d7061699718cd1235cf9ea9b79a8e926483c86
Kamil Gurgul <kamgurgul@gmail.com>
2024-05-20 13:20:22 +0200

    Add adaptive layout deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,6 +119,8 @@ dependencies {
     androidTestImplementation(composeBom)
     implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive.navigation)
+    implementation(libs.androidx.compose.material3.windowSizeClass)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     debugImplementation(libs.androidx.compose.ui.tooling)
