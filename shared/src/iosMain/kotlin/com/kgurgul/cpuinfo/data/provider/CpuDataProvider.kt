3b586727cdfc0a7633eddc85cb25e629e5716458
Kamil Gurgul <kamgurgul@gmail.com>
2024-06-27 11:56:10 +0200

    [KMP] iOS add abi detection based on Kotlin native impl

--- shared/src/iosMain/kotlin/com/kgurgul/cpuinfo/data/provider/CpuDataProvider.kt
+++ shared/src/iosMain/kotlin/com/kgurgul/cpuinfo/data/provider/CpuDataProvider.kt
@@ -3,14 +3,17 @@ package com.kgurgul.cpuinfo.data.provider
 import org.koin.core.annotation.Factory
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import kotlin.experimental.ExperimentalNativeApi
+import kotlin.native.Platform.cpuArchitecture
 
 @Factory
 actual class CpuDataProvider actual constructor() : KoinComponent {
 
     private val cpuDataNativeProvider: CpuDataNativeProvider by inject()
 
+    @OptIn(ExperimentalNativeApi::class)
     actual fun getAbi(): String {
-        return "iOS"
+        return cpuArchitecture.name
     }
 
     actual fun getNumberOfCores(): Int {
