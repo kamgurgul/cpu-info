2a83e8fcdaf5a21849f73af31e9ef79869feadce
Kamil Gurgul <kamgurgul@gmail.com>
2024-06-28 08:35:30 +0200

    [KMP] Handle theme switcher

--- shared/src/iosMain/kotlin/com/kgurgul/cpuinfo/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/kgurgul/cpuinfo/MainViewController.kt
@@ -1,9 +1,24 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kgurgul.cpuinfo.features.HostScreen
+import com.kgurgul.cpuinfo.features.HostViewModel
+import com.kgurgul.cpuinfo.ui.shouldUseDarkTheme
 import com.kgurgul.cpuinfo.ui.theme.CpuInfoTheme
+import org.koin.compose.viewmodel.koinViewModel
 
 fun MainViewController() = ComposeUIViewController {
-    CpuInfoTheme {
-        HostScreen()
+    IOSComposeApp()
+}
+
+@Composable
+fun IOSComposeApp(hostViewModel: HostViewModel = koinViewModel()) {
+    val uiState by hostViewModel.uiStateFlow.collectAsStateWithLifecycle()
+    val darkTheme = shouldUseDarkTheme(uiState)
+    CpuInfoTheme(
+        useDarkTheme = darkTheme
+    ) {
+        HostScreen(hostViewModel)
     }
 }
