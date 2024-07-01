ddb5b8914c0c7e81222706db85858eba039f754a
Kamil Gurgul <kamgurgul@gmail.com>
2024-07-01 09:28:35 +0200

    [KMP] Get GLES version on iOS

--- shared/src/iosMain/kotlin/com/kgurgul/cpuinfo/data/provider/GpuDataProvider.kt
+++ shared/src/iosMain/kotlin/com/kgurgul/cpuinfo/data/provider/GpuDataProvider.kt
@@ -1,12 +1,24 @@
 package com.kgurgul.cpuinfo.data.provider
 
+import kotlinx.cinterop.memScoped
 import org.koin.core.annotation.Factory
+import platform.EAGL.EAGLContext
+import platform.EAGL.kEAGLRenderingAPIOpenGLES3
 
 @Factory
 actual class GpuDataProvider actual constructor() {
 
     actual fun getGlEsVersion(): String {
-        return "GLEs version"
+        return memScoped {
+            val context = runCatching {
+                EAGLContext(kEAGLRenderingAPIOpenGLES3)
+            }.getOrNull()
+            if (context != null) {
+                "3.0"
+            } else {
+                "2.0"
+            }
+        }
     }
 
     actual fun getVulkanVersion(): String {
