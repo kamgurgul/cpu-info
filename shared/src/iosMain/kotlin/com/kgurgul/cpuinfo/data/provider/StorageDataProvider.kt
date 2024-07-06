a72566511b08bb7d10d3483caedfe865c055a747
Kamil Gurgul <kamgurgul@gmail.com>
2024-07-06 16:27:03 +0200

    [KMP] Add iOS storage info

--- shared/src/iosMain/kotlin/com/kgurgul/cpuinfo/data/provider/StorageDataProvider.kt
+++ shared/src/iosMain/kotlin/com/kgurgul/cpuinfo/data/provider/StorageDataProvider.kt
@@ -1,12 +1,33 @@
 package com.kgurgul.cpuinfo.data.provider
 
 import com.kgurgul.cpuinfo.domain.model.StorageItem
+import com.kgurgul.cpuinfo.shared.IosHardwareDataProvider
+import com.kgurgul.cpuinfo.shared.Res
+import com.kgurgul.cpuinfo.shared.baseline_folder_special_24
+import com.kgurgul.cpuinfo.shared.internal
 import org.koin.core.annotation.Factory
 
 @Factory
 actual class StorageDataProvider actual constructor() {
 
     actual fun getStorageInfo(): List<StorageItem> {
-        return emptyList()
+        return listOfNotNull(getInternalStorage())
+    }
+
+    private fun getInternalStorage(): StorageItem? {
+        val storageTotal = IosHardwareDataProvider.sharedInstance().getTotalDiskSpaceInBytes()
+        val availableSpace = IosHardwareDataProvider.sharedInstance().getFreeDiskSpaceInBytes()
+        val storageUsed = storageTotal - availableSpace
+        return if (storageTotal > 0) {
+            StorageItem(
+                id = 0L,
+                label = Res.string.internal,
+                iconDrawable = Res.drawable.baseline_folder_special_24,
+                storageTotal = storageTotal,
+                storageUsed = storageUsed,
+            )
+        } else {
+            null
+        }
     }
 }
--- shared/src/nativeInterop/cinterop/cpuinfoframework/IosHardwareDataProvider.swift
+++ shared/src/nativeInterop/cinterop/cpuinfoframework/IosHardwareDataProvider.swift
@@ -8,5 +8,27 @@ import Foundation
         return os_proc_available_memory()
     }
 
+    @objc public func getTotalDiskSpaceInBytes() -> Int64 {
+        do {
+            let systemAttributes = try FileManager.default
+                .attributesOfFileSystem(forPath: NSHomeDirectory() as String)
+            let space = (systemAttributes[FileAttributeKey.systemSize] as?NSNumber)?.int64Value
+            return space!
+        } catch {
+            return 0
+        }
+    }
+
+    @objc public func getFreeDiskSpaceInBytes() -> Int64 {
+        do {
+            let systemAttributes = try FileManager.default
+                .attributesOfFileSystem(forPath: NSHomeDirectory() as String)
+            let freeSpace = (systemAttributes[FileAttributeKey.systemFreeSize] as? NSNumber)?.int64Value
+            return freeSpace!
+        } catch {
+            return 0
+        }
+    }
+
     private override init() {}
 }
