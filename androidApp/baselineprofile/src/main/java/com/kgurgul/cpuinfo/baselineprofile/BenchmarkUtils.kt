47a177887f76444df5d04fea957892c49e62ee4e
Kamil Gurgul <kamgurgul@gmail.com>
2024-07-23 06:02:09 +0200

    [ANDROID] Update baselineprofile

--- androidApp/baselineprofile/src/main/java/com/kgurgul/cpuinfo/baselineprofile/BenchmarkUtils.kt
+++ androidApp/baselineprofile/src/main/java/com/kgurgul/cpuinfo/baselineprofile/BenchmarkUtils.kt
@@ -2,10 +2,12 @@ package com.kgurgul.cpuinfo.baselineprofile
 
 import androidx.benchmark.macro.MacrobenchmarkScope
 import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiScrollable
+import androidx.test.uiautomator.UiSelector
 import androidx.test.uiautomator.Until
 
 fun MacrobenchmarkScope.waitForAsyncContent() {
     device.wait(Until.hasObject(By.res("cpu_info_lazy_column")), 5_000)
-    val contentList = device.findObject(By.res("cpu_info_lazy_column"))
-    contentList.wait(Until.hasObject(By.res("cpu_info_socket_name")), 5_000)
+    val element = UiScrollable(UiSelector().scrollable(true))
+    element.scrollToEnd(3)
 }
