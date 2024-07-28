9fbca97566f6cb980d95f87cbcca3c7478d44a0f
Kamil Gurgul <kamgurgul@gmail.com>
2024-07-28 08:51:06 +0200

    [DESKTOP] Add initial desktop icons

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -25,19 +25,18 @@ compose.desktop {
             packageName = "CPUInfo"
             packageVersion = "1.0.0"
 
-            /* val iconsRoot = project.file("desktop-icons")
+            val iconsRoot = project.file("desktop-icons")
              macOS {
                  iconFile.set(iconsRoot.resolve("icon-mac.icns"))
              }
              windows {
                  iconFile.set(iconsRoot.resolve("icon-windows.ico"))
-                 menuGroup = "Compose Examples"
-                 // see https://wixtoolset.org/documentation/manual/v3/howtos/general/generate_guids.html
-                 upgradeUuid = "18159995-d967-4CD2-8885-77BFA97CFA9F"
+                 menuGroup = "CPU Info"
+                 upgradeUuid = "d4a688eb-758f-44e8-81bb-8367880c5c95"
              }
              linux {
                  iconFile.set(iconsRoot.resolve("icon-linux.png"))
-             }*/
+             }
         }
     }
 }
--- desktopApp/src/jvmMain/kotlin/com/kgurgul/cpuinfo/main.kt
+++ desktopApp/src/jvmMain/kotlin/com/kgurgul/cpuinfo/main.kt
@@ -3,6 +3,9 @@ package com.kgurgul.cpuinfo
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.kgurgul.cpuinfo.di.initKoin
+import com.kgurgul.cpuinfo.shared.Res
+import com.kgurgul.cpuinfo.shared.desktop_icon
+import org.jetbrains.compose.resources.painterResource
 
 fun main() {
     initKoin()
@@ -10,6 +13,7 @@ fun main() {
         Window(
             onCloseRequest = ::exitApplication,
             title = "CPU Info",
+            icon = painterResource(Res.drawable.desktop_icon),
         ) {
             DesktopApp()
         }
