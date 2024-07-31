44b380230ad67ed446e0beafae6d99c051206c77
Kamil Gurgul <kamgurgul@gmail.com>
2024-07-31 11:28:21 +0200

    [DESKTOP] Add memory data provider

--- shared/src/desktopMain/kotlin/com/kgurgul/cpuinfo/data/provider/RamDataProvider.kt
+++ shared/src/desktopMain/kotlin/com/kgurgul/cpuinfo/data/provider/RamDataProvider.kt
@@ -2,16 +2,21 @@ package com.kgurgul.cpuinfo.data.provider
 
 import org.koin.core.annotation.Factory
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import oshi.SystemInfo
 
 @Factory
 actual class RamDataProvider actual constructor() : KoinComponent {
 
+    private val systemInfo: SystemInfo by inject()
+    private val memory = systemInfo.hardware.memory
+
     actual fun getTotalBytes(): Long {
-        return -1L
+        return memory.total
     }
 
     actual fun getAvailableBytes(): Long {
-        return -1L
+        return memory.available
     }
 
     actual fun getThreshold(): Long {
--- shared/src/desktopMain/kotlin/com/kgurgul/cpuinfo/data/provider/StorageDataProvider.kt
+++ shared/src/desktopMain/kotlin/com/kgurgul/cpuinfo/data/provider/StorageDataProvider.kt
@@ -2,9 +2,15 @@ package com.kgurgul.cpuinfo.data.provider
 
 import com.kgurgul.cpuinfo.domain.model.StorageItem
 import org.koin.core.annotation.Factory
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import oshi.SystemInfo
 
 @Factory
-actual class StorageDataProvider actual constructor() {
+actual class StorageDataProvider actual constructor() : KoinComponent {
+
+    private val systemInfo: SystemInfo by inject()
+    private val diskStores = systemInfo.hardware.diskStores
 
     actual fun getStorageInfo(): List<StorageItem> {
         return emptyList()
