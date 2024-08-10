c018363dfac384384d13048280d4e9f31393a94f
Kamil Gurgul <kamgurgul@gmail.com>
2024-08-10 19:55:29 +0200

    [DESKTOP] Update packageName

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -22,7 +22,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "CPUInfo"
+            packageName = "CPU Info"
             packageVersion = "1.0.0"
 
             val iconsRoot = project.file("desktop-icons")
