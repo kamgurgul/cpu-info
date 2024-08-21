510391593c8f05fc0fa080fd6d8797e49537522d
Kamil Gurgul <kamgurgul@gmail.com>
2024-08-21 08:40:52 +0200

    [DESKTOP] Workaround RPI issue

--- desktopApp/src/jvmMain/kotlin/com/kgurgul/cpuinfo/main.kt
+++ desktopApp/src/jvmMain/kotlin/com/kgurgul/cpuinfo/main.kt
@@ -6,9 +6,17 @@ import com.kgurgul.cpuinfo.di.initKoin
 import com.kgurgul.cpuinfo.shared.Res
 import com.kgurgul.cpuinfo.shared.desktop_icon
 import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.skia.DirectContext
 
 fun main() {
     initKoin()
+    if (System.getProperty("os.name") == "Linux") {
+        try {
+            DirectContext.makeGL().flush().close()
+        } catch (e: Throwable) {
+            System.setProperty("skiko.renderApi", "SOFTWARE")
+        }
+    }
     return application {
         Window(
             onCloseRequest = ::exitApplication,
