f243e809971160387da7230a7557c8c9dbd9c04f
Kamil Gurgul <kamgurgul@gmail.com>
2024-10-13 07:32:26 +0200

    [Shared] Add elevation transition to DraggableBox

--- shared/src/commonMain/kotlin/com/kgurgul/cpuinfo/ui/components/DraggableBox.kt
+++ shared/src/commonMain/kotlin/com/kgurgul/cpuinfo/ui/components/DraggableBox.kt
@@ -1,12 +1,14 @@
 package com.kgurgul.cpuinfo.ui.components
 
 import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.rememberTransition
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.Stable
@@ -18,11 +20,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import kotlin.math.roundToInt
 
 @Composable
@@ -50,6 +54,10 @@ fun DraggableBox(
             if (targetIsRevealed) offsetX - actionRowOffset else -offsetX
         },
     )
+    val elevationTransition by transition.animateDp(
+        label = "boxElevationTransition",
+        targetValueByState = { if (isRevealed) 40.dp else 0.dp }
+    )
     Box(
         contentAlignment = Alignment.CenterEnd,
         modifier = Modifier
@@ -66,6 +74,10 @@ fun DraggableBox(
             modifier = Modifier
                 .fillMaxWidth()
                 .offset { IntOffset((offsetX + offsetTransition).roundToInt(), 0) }
+                .shadow(
+                    elevation = elevationTransition,
+                    spotColor = MaterialTheme.colorScheme.onBackground
+                )
                 .pointerInput(key) {
                     detectHorizontalDragGestures { change, dragAmount ->
                         val original = Offset(offsetX, 0f)
