62ed89eb54654c284fdcc6f6091f119ff1ee0d44
Kamil Gurgul <kamgurgul@gmail.com>
2024-10-28 06:54:20 +0100

    [WASM] Add screen info

--- shared/src/wasmJsMain/kotlin/com/kgurgul/cpuinfo/data/provider/ScreenDataProvider.wasm.kt
+++ shared/src/wasmJsMain/kotlin/com/kgurgul/cpuinfo/data/provider/ScreenDataProvider.wasm.kt
@@ -1,5 +1,6 @@
 package com.kgurgul.cpuinfo.data.provider
 
+import kotlinx.browser.window
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 import org.koin.core.component.KoinComponent
@@ -7,7 +8,12 @@ import org.koin.core.component.KoinComponent
 actual class ScreenDataProvider actual constructor() : KoinComponent {
 
     actual suspend fun getData(): List<Pair<String, String>> {
-        return emptyList()
+        return buildList {
+            add("Inner width" to "${window.innerWidth}px")
+            add("Inner height" to "${window.innerHeight}px")
+            add("Screen available width" to "${window.screen.availWidth}px")
+            add("Screen available height" to "${window.screen.availHeight}px")
+        }
     }
 
     actual fun getOrientationFlow(): Flow<String> = emptyFlow()
