511f6931b3d08cb1f0494c000f4e03edc7d352e5
Kamil Gurgul <kamgurgul@gmail.com>
2024-11-14 08:16:56 +0100

    [SHARED] Adjust processes VM

--- shared/src/commonMain/kotlin/com/kgurgul/cpuinfo/features/processes/ProcessesViewModel.kt
+++ shared/src/commonMain/kotlin/com/kgurgul/cpuinfo/features/processes/ProcessesViewModel.kt
@@ -8,13 +8,11 @@ import com.kgurgul.cpuinfo.domain.model.sortOrderFromBoolean
 import com.kgurgul.cpuinfo.domain.observable.ProcessesDataObservable
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
-import kotlinx.collections.immutable.toPersistentList
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.collections.immutable.toImmutableList
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class ProcessesViewModel(
@@ -22,34 +20,29 @@ class ProcessesViewModel(
     private val userPreferencesRepository: IUserPreferencesRepository,
 ) : ViewModel() {
 
-    private val _uiStateFlow = MutableStateFlow(UiState())
-    val uiStateFlow = _uiStateFlow.asStateFlow()
-
-    init {
-        userPreferencesRepository.userPreferencesFlow
-            .flatMapLatest { userPreferences ->
-                _uiStateFlow.update {
-                    it.copy(isSortAscending = userPreferences.isProcessesSortingAscending)
-                }
-                processesDataObservable.observe(
-                    ProcessesDataObservable.Params(
-                        sortOrder = sortOrderFromBoolean(
-                            userPreferences.isProcessesSortingAscending,
-                        ),
-                    ),
-                )
-            }.onEach(::handleProcessesResult)
-            .launchIn(viewModelScope)
-    }
+    val uiStateFlow = userPreferencesRepository.userPreferencesFlow
+        .flatMapLatest { userPreferences ->
+            val isSortAscending = userPreferences.isProcessesSortingAscending
+            processesDataObservable.observe(
+                ProcessesDataObservable.Params(
+                    sortOrder = sortOrderFromBoolean(isSortAscending),
+                ),
+            ).map { processes ->
+                isSortAscending to processes
+            }
+        }.map {
+            val (isSortAscending, processes) = it
+            UiState(
+                isLoading = false,
+                processes = processes.toImmutableList(),
+                isSortAscending = isSortAscending,
+            )
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), UiState())
 
     fun onSortOrderChange(isAscending: Boolean) {
         viewModelScope.launch {
             userPreferencesRepository.setProcessesSortingOrder(isAscending)
         }
-    }
-
-    private fun handleProcessesResult(processes: List<ProcessItem>) {
-        _uiStateFlow.update { it.copy(isLoading = false, processes = processes.toPersistentList()) }
     }
 
     data class UiState(
