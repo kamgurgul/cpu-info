825b2f75bacd94d13874e574c4c03259195cec51
Kamil Gurgul <kamgurgul@gmail.com>
2024-12-28 08:09:01 +0100

    [TV] Simplify TV activity

--- androidApp/src/androidMain/kotlin/com/kgurgul/cpuinfo/TvActivity.kt
+++ androidApp/src/androidMain/kotlin/com/kgurgul/cpuinfo/TvActivity.kt
@@ -1,19 +1,15 @@
 package com.kgurgul.cpuinfo
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -22,8 +18,6 @@ import com.kgurgul.cpuinfo.features.HostViewModel
 import com.kgurgul.cpuinfo.features.TvHostScreen
 import com.kgurgul.cpuinfo.ui.shouldUseDarkTheme
 import com.kgurgul.cpuinfo.ui.theme.CpuInfoTheme
-import com.kgurgul.cpuinfo.ui.theme.DarkColors
-import com.kgurgul.cpuinfo.ui.theme.LightColors
 import com.kgurgul.cpuinfo.ui.theme.darkPrimary
 import com.kgurgul.cpuinfo.ui.theme.lightPrimary
 import kotlinx.coroutines.flow.collect
@@ -61,30 +55,8 @@ class TvActivity : AppCompatActivity() {
                 )
                 onDispose {}
             }
-            val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
-            val colors = when {
-                dynamicColor && darkTheme -> {
-                    val dynamicDarkColors = dynamicDarkColorScheme(LocalContext.current)
-                    DarkColors.copy(
-                        secondary = dynamicDarkColors.primaryContainer,
-                        onSecondary = dynamicDarkColors.onPrimaryContainer,
-                    )
-                }
-
-                dynamicColor && !darkTheme -> {
-                    val dynamicLightColors = dynamicLightColorScheme(LocalContext.current)
-                    LightColors.copy(
-                        secondary = dynamicLightColors.secondary,
-                        onSecondary = dynamicLightColors.onSecondary,
-                    )
-                }
-
-                darkTheme -> DarkColors
-                else -> LightColors
-            }
             CpuInfoTheme(
                 useDarkTheme = darkTheme,
-                colors = colors,
             ) {
                 TvHostScreen(
                     viewModel = viewModel,
