508fbb36795610d97ab88d4a3e038463220bb620
Kamil Gurgul <kamgurgul@gmail.com>
2024-12-31 09:58:06 +0100

    [WEAR] Base for information screen

--- wearOsApp/src/androidMain/kotlin/com/kgurgul/cpuinfo/wear/features/WearHostScreen.kt
+++ wearOsApp/src/androidMain/kotlin/com/kgurgul/cpuinfo/wear/features/WearHostScreen.kt
@@ -5,21 +5,13 @@ package com.kgurgul.cpuinfo.wear.features
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Build
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.wear.compose.material.ChipDefaults
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.TitleCard
-import androidx.wear.compose.material.dialog.Dialog
 import androidx.wear.compose.navigation.SwipeDismissableNavHost
 import androidx.wear.compose.navigation.composable
 import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
@@ -30,8 +22,6 @@ import com.google.android.horologist.compose.layout.ScalingLazyColumnDefaults
 import com.google.android.horologist.compose.layout.ScalingLazyColumnDefaults.ItemType
 import com.google.android.horologist.compose.layout.ScreenScaffold
 import com.google.android.horologist.compose.layout.rememberResponsiveColumnState
-import com.google.android.horologist.compose.material.AlertContent
-import com.google.android.horologist.compose.material.Button
 import com.google.android.horologist.compose.material.Chip
 import com.google.android.horologist.compose.material.ListHeaderDefaults.firstItemPadding
 import com.google.android.horologist.compose.material.ResponsiveListHeader
@@ -45,6 +35,7 @@ import com.kgurgul.cpuinfo.shared.information
 import com.kgurgul.cpuinfo.shared.menu
 import com.kgurgul.cpuinfo.shared.settings
 import com.kgurgul.cpuinfo.shared.temp
+import com.kgurgul.cpuinfo.wear.features.information.WearInfoContainerScreen
 import com.kgurgul.cpuinfo.wear.theme.WearAppTheme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
@@ -60,11 +51,13 @@ fun WearHostScreen() {
             ) {
                 composable(WearHostScreen.Menu.route) {
                     MenuScreen(
-                        onInformationClicked = { navController.navigate("list") }
+                        onInformationClicked = {
+                            navController.navigate(WearHostScreen.Information.route)
+                        }
                     )
                 }
-                composable("list") {
-                    ListScreen()
+                composable(WearHostScreen.Information.route) {
+                    WearInfoContainerScreen()
                 }
             }
         }
@@ -162,101 +155,5 @@ fun MenuScreen(
 
 sealed class WearHostScreen(val route: String) {
     data object Menu : WearHostScreen("menu")
-}
-
-@Composable
-fun ListScreen() {
-    var showDialog by remember { mutableStateOf(false) }
-
-    /*
-     * Specifying the types of items that appear at the start and end of the list ensures that the
-     * appropriate padding is used.
-     */
-    val columnState = rememberResponsiveColumnState(
-        contentPadding = ScalingLazyColumnDefaults.padding(
-            first = ItemType.Text,
-            last = ItemType.SingleButton
-        )
-    )
-
-    ScreenScaffold(scrollState = columnState) {
-        /*
-         * The Horologist [ScalingLazyColumn] takes care of the horizontal and vertical
-         * padding for the list, so there is no need to specify it, as in the [GreetingScreen]
-         * composable.
-         */
-        ScalingLazyColumn(
-            columnState = columnState
-        ) {
-            item {
-                ResponsiveListHeader(contentPadding = firstItemPadding()) {
-                    Text(text = "Header")
-                }
-            }
-            item {
-                TitleCard(title = { Text("Example Title") }, onClick = { }) {
-                    Text("Example Content\nMore Lines\nAnd More")
-                }
-            }
-            item {
-                Chip(label = "Example Chip", onClick = { })
-            }
-            item {
-                Button(
-                    imageVector = Icons.Default.Build,
-                    contentDescription = "Example Button",
-                    onClick = { showDialog = true }
-                )
-            }
-        }
-    }
-
-    SampleDialog(
-        showDialog = showDialog,
-        onDismiss = { showDialog = false },
-        onCancel = {},
-        onOk = {}
-    )
-}
-
-@Composable
-fun SampleDialog(
-    showDialog: Boolean,
-    onDismiss: () -> Unit,
-    onCancel: () -> Unit,
-    onOk: () -> Unit
-) {
-    val state = rememberResponsiveColumnState()
-
-    Dialog(
-        showDialog = showDialog,
-        onDismissRequest = onDismiss,
-        scrollState = state.state
-    ) {
-        SampleDialogContent(onCancel, onDismiss, onOk)
-    }
-}
-
-@Composable
-fun SampleDialogContent(
-    onCancel: () -> Unit,
-    onDismiss: () -> Unit,
-    onOk: () -> Unit
-) {
-    AlertContent(
-        icon = {},
-        title = "Title",
-        onCancel = {
-            onCancel()
-            onDismiss()
-        },
-        onOk = {
-            onOk()
-            onDismiss()
-        }
-    ) {
-        item {
-            Text(text = "An unknown error occurred during the request.")
-        }
-    }
+    data object Information : WearHostScreen("information")
 }
