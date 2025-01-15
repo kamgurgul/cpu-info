6aace451df67311fb1691cedc00a24ad52b05730
Kamil Gurgul <kamgurgul@gmail.com>
2025-01-15 07:50:06 +0100

    [DESKTOP] Bump desktop version

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -23,7 +23,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Pkg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "CPU-Info"
-            packageVersion = "1.3.0"
+            packageVersion = "1.4.0"
 
             val iconsRoot = project.file("desktop-icons")
             macOS {
