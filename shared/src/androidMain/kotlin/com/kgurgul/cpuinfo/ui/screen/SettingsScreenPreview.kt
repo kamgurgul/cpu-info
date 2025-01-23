e9305be96f630cf8abcb7640a2b9d8c84f619aa7
Kamil Gurgul <kamgurgul@gmail.com>
2025-01-23 09:41:48 +0100

    [SHARED] Fix preview

--- shared/src/androidMain/kotlin/com/kgurgul/cpuinfo/ui/screen/SettingsScreenPreview.kt
+++ shared/src/androidMain/kotlin/com/kgurgul/cpuinfo/ui/screen/SettingsScreenPreview.kt
@@ -14,6 +14,7 @@ fun SettingsScreenPreview() {
             uiState = SettingsViewModel.UiState(),
             onTemperatureOptionClicked = {},
             onThemeOptionClicked = {},
+            onLicensesClicked = {},
         )
     }
 }
