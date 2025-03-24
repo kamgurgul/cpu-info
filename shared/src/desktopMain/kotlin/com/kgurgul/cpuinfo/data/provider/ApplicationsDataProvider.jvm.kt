aeb5a1633369583a7674626f05eef6b1917012f1
Kamil Gurgul <kamgurgul@gmail.com>
2025-03-24 06:51:27 +0100

    [DESKTOP] Use OSHI for OS app list on JVM

--- shared/src/desktopMain/kotlin/com/kgurgul/cpuinfo/data/provider/ApplicationsDataProvider.jvm.kt
+++ shared/src/desktopMain/kotlin/com/kgurgul/cpuinfo/data/provider/ApplicationsDataProvider.jvm.kt
@@ -1,14 +1,30 @@
 package com.kgurgul.cpuinfo.data.provider
 
 import com.kgurgul.cpuinfo.domain.model.ExtendedApplicationData
+import kotlin.getValue
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import oshi.SystemInfo
 
 actual class ApplicationsDataProvider actual constructor() :
     IApplicationsDataProvider,
     KoinComponent {
 
+    private val systemInfo: SystemInfo by inject()
+
     actual override fun getInstalledApplications(withSystemApps: Boolean): List<ExtendedApplicationData> {
-        return emptyList()
+        return systemInfo.operatingSystem.installedApplications
+            .distinctBy { it.name }
+            .map {
+                ExtendedApplicationData(
+                    name = it.name,
+                    packageName = it.name,
+                    versionName = it.version,
+                    nativeLibs = emptyList(),
+                    hasNativeLibs = false,
+                    appIconUri = "",
+                )
+            }
     }
 
     actual override fun areApplicationsSupported() = false
