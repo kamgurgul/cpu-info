cba7cc879c1855e3786f17c67358cb3ebdd74934
Kamil Gurgul <kamgurgul@gmail.com>
2026-01-28 07:02:13 +0100

    [SHARED] Update libs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,10 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 include(":androidApp")
 include(":androidApp:baselineprofile")
 include(":desktopApp")
