8ad1f076311f348fc712e43c820c1499aaeaeae1
Kamil Gurgul <kamgurgul@gmail.com>
2026-05-12 07:02:19 +0200

    [SHARED] Update android config

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -57,6 +57,7 @@ android {
         release {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
