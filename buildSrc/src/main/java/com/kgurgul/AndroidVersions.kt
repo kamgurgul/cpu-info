3940d65eda475c0b38c22ee09e1a34bb5dbafb6f
Kamil Gurgul <kamgurgul@gmail.com>
2026-06-16 07:55:20 +0200

    [SHARED] Update libs and Android SDK

--- buildSrc/src/main/java/com/kgurgul/AndroidVersions.kt
+++ buildSrc/src/main/java/com/kgurgul/AndroidVersions.kt
@@ -19,8 +19,8 @@ object AndroidVersions {
 
     const val MIN_SDK = 23
     const val WEAR_MIN_SDK = 26
-    const val TARGET_SDK = 36
-    const val COMPILE_SDK = 36
+    const val TARGET_SDK = 37
+    const val COMPILE_SDK = 37
 
     private const val VERSION_MAJOR = 6
     private const val VERSION_MINOR = 7
