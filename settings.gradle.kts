4f417e21f072f942efe3700891fe6ba5ca5a43a9
Kamil Gurgul <kamgurgul@gmail.com>
2026-09-12 13:06:07 +0200

    [SHARED] Update libs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("io.github.ben-manes.versions.settings") version "0.61.0"
+    id("io.github.ben-manes.versions.settings") version "0.62.0"
 }
 
 include(":androidApp")
